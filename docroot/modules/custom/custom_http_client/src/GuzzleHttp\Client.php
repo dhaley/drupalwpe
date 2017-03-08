75acd1e5367287725e8460fa9e14dc4fc03281f3
Haley <dhaley@dhaley-30197s.nrel.gov>
2017-03-08 14:50:16 -0700

    Attempted to generate service.

---
+++ docroot/modules/custom/custom_http_client/src/GuzzleHttp\Client.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Drupal\custom_http_client;
+
+
+/**
+ * Class GuzzleHttp\Client.
+ *
+ * @package Drupal\custom_http_client
+ */
+class GuzzleHttp\Client implements GuzzleHttp\ClientInterface {
+  /**
+   * Constructor.
+   */
+  public function __construct() {
+
+  }
+
+}
---
+++ docroot/modules/custom/custom_http_client/src/GuzzleHttp\ClientInterface.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Drupal\custom_http_client;
+
+/**
+ * Interface GuzzleHttp\ClientInterface.
+ *
+ * @package Drupal\custom_http_client
+ */
+interface GuzzleHttp\ClientInterface {
+
+
+}
